036327504ef1c44e14318dab72cf42e996657bbc
AnastasiiiaMazur <nmazur.dev@gmail.com>
2026-09-17 17:26:43 +0100

    tax and product set up

--- src/main/java/com/mc/flooringmastery/dto/Product.java
+++ src/main/java/com/mc/flooringmastery/dto/Product.java
@@ -1,4 +1,22 @@
 package com.mc.flooringmastery.dto;
 
+import java.math.BigDecimal;
+
 public class Product {
+
+    private String productType;
+    private BigDecimal costPerSquareFoot;
+    private BigDecimal labourCostPerSquareFoot;
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public BigDecimal getCostPerSquareFoot() {
+        return costPerSquareFoot;
+    }
+
+    public BigDecimal getLabourCostPerSquareFoot() {
+        return labourCostPerSquareFoot;
+    }
 }
--- src/main/java/com/mc/flooringmastery/dto/Tax.java
+++ src/main/java/com/mc/flooringmastery/dto/Tax.java
@@ -1,4 +1,22 @@
 package com.mc.flooringmastery.dto;
 
+import java.math.BigDecimal;
+
 public class Tax {
+
+    private String state;
+    private String stateAbr;
+    private BigDecimal taxRate;
+
+    public String getState() {
+        return state;
+    }
+
+    public String getStateAbr() {
+        return stateAbr;
+    }
+
+    public BigDecimal getTaxRate() {
+        return taxRate;
+    }
 }
